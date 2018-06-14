bfd2103b74e6125ef976f8e49361a8dbec8a8538
Taha Jalili <tahajalili@gmail.com>
2018-06-14 14:20:38 +0430

    SOME changes in code

--- Bitcoin-price-notify/btc-price-alarm.py
+++ Bitcoin-price-notify/btc-price-alarm.py
@@ -1,7 +1,7 @@
 import requests
 from kavenegar import *
 
-api = KavenegarAPI('4A6E43704C687458316F7A2B5656564E3750644B3055677A6F4C7164774E6F47')
+api = KavenegarAPI('YOUR_API_KEY')
 
 r = requests.get('https://api.coinbase.com/v2/prices/buy?currency=USD')
 price = float(r.json()['data']['amount'])
@@ -9,8 +9,8 @@ price = float(r.json()['data']['amount'])
 params = {
 
 'sender': '100065995',
-'receptor':'09125278727',
+'receptor':'NUMBER_TO_RECEIVE',
 'message' : 'Bitcoin price: '+str(price)+'$'
 }
 response = api.sms_send(params)
-#print('At this moment BitCoin is %.2f dollars.' % price)
+
