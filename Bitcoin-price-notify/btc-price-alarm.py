913e6cf1570ffbeea5da51e5513e0a1117b63422
Taha Jalili <tahajalili@gmail.com>
2018-09-01 20:38:15 +0430

    New Changes for btc price

--- Bitcoin-price-notify/btc-price-alarm.py
+++ Bitcoin-price-notify/btc-price-alarm.py
@@ -1,16 +1,26 @@
 import requests
-from kavenegar import *
-
-api = KavenegarAPI('YOUR_API_KEY')
+import notify2
 
 r = requests.get('https://api.coinbase.com/v2/prices/buy?currency=USD')
 price = float(r.json()['data']['amount'])
 
-params = {
+notify2.init("Bitcoin Price")
+n = notify2.Notification('Live Bitcoin Price', 'BTC => '+str(price)+'$')
+n.show()
 
-'sender': '100065995',
-'receptor':'NUMBER_TO_RECEIVE',
-'message' : 'Bitcoin price: '+str(price)+'$'
-}
-response = api.sms_send(params)
+
+
+# from kavenegar import *
+
+# api = KavenegarAPI('YOUR_API_KEY')
+
+
+
+# params = {
+
+# 'sender': '100065995',
+# 'receptor':'NUMBER_TO_RECEIVE',
+# 'message' : 'Bitcoin price: '+str(price)+'$'
+# }
+# response = api.sms_send(params)
 
